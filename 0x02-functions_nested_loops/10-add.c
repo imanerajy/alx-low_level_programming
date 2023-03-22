4023b1e252e91fb9dcb2c1e29dcdad966a22e4fb
imane <rajyimane@gmail.com>
2023-03-22 15:28:42 -0700

    add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,34 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * times_table - prints the 9 times table, starting with 0
+ * add - adds two integers
+ * @i: integer
+ * @j: integer
+ * Return: the result
  */
 
-void times_table(void)
+int add(int i, int j)
 {
-	int a, b, c;
-
-	for (a = 0 ; a < 10 ; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
-			if (b == 0)
-				_putchar(c + '0');
-			if (b != 0 && c < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
-			}
-			else if (c >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+	return (i + j);
 }
